f7a6ca3220e3fd7ea1f3d4f43b27ec70d0c668be
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-04-09 15:06:10 +0600

    navbar done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Slider from "../../components/slider/Slider";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+        {
+            path: "login",
+            element:<Login></Login>
+        },
+        {
+            path: "register",
+            element: <Register></Register>
+        }
+      ]
+    },
+  ]);
+  export default router
